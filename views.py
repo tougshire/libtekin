2a1441e706b36f67700291290f4833c7e3f2b1ae
tougshire <tougshire@tougshire.com>
2022-02-01 14:55:07 -0500

    changes to accomodate changes in tougshire_vistas

--- views.py
+++ views.py
@@ -1,17 +1,22 @@
 import json
 
+from django.contrib import messages
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.core.exceptions import FieldError, ObjectDoesNotExist
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
-from django.core.exceptions import FieldError, ObjectDoesNotExist
-from .forms import EntityForm, ItemForm, LocationForm, MmodelForm, MmodelCategoryForm, ItemItemNoteFormset
-from .models import Condition, Entity, Item, ItemNote, Location, Mmodel, MmodelCategory, Role, History
 from tougshire_vistas.models import Vista
-from tougshire_vistas.views import get_vista_object
+from tougshire_vistas.views import make_vista, retrieve_vista
+
+from .forms import (EntityForm, ItemForm, ItemItemNoteFormset, LocationForm,
+                    MmodelCategoryForm, MmodelForm)
+from .models import (Condition, Entity, History, Item, ItemNote, Location,
+                     Mmodel, MmodelCategory, Role)
+
 
 def update_history(form, modelname, object, user):
     for fieldname in form.changed_data:
@@ -156,87 +161,109 @@ class ItemSoftDelete(PermissionRequiredMixin, UpdateView):
 class ItemList(PermissionRequiredMixin, ListView):
     permission_required = 'libtekin.view_item'
     model = Item
-    filter_object = {}
-    exclude_object = {}
-    order_by = []
-    order_by_fields=[]
 
-    for fieldname in ['common_name', 'mmodel', 'primary_id', 'serial_number','service_number', 'latest_inventory']:
-        order_by_fields.append(
-            { 'name':fieldname, 'label':Item._meta.get_field(fieldname).verbose_name.title() }
-        )
-        order_by_fields.append(
-            { 'name':'-' + fieldname, 'label':'{} reverse'.format(Item._meta.get_field(fieldname).verbose_name.title()) }
-        )
+    def setup(self, request, *args, **kwargs):
 
-    combined_text_search=""
-    combined_text_fields=[
-            'common_name',
-            'mmodel__model_name',
-            'mmodel__category__name',
-            'mmodel__brand',
-            'primary_id',
-            'serial_number',
-            'service_number',
-            'asset_number',
-            'barcode',
-            'network_name',
-            'assignee__friendly_name',
-            'assignee__full_name',
-            'borrower__friendly_name',
-            'borrower__full_name',
-            'owner__friendly_name',
-            'owner__full_name',
-            'home__short_name',
-            'home__full_name',
-            'location__short_name',
-            'location__full_name',
-    ]
+        self.vista_settings={
+            'text_fields_available':[],
+            'filter_fields_available':{},
+            'order_by_fields_available':[],
+            'columns_available':[]
+        }
 
-    filter_fields = {
-        'in':['mmodel', 'mmodel__category', 'condition', 'role', 'location', 'home'],
-        'lte':['latest_inventory'],
-        'gte':['latest_inventory'],
-    }
-    showable_columns = []
-    show_columns = []
-    for fieldname in [
-            'common_name',
+        self.vista_settings['text_fields_available']=[
+                'common_name',
+                'mmodel__model_name',
+                'mmodel__category__name',
+                'mmodel__brand',
+                'primary_id',
+                'serial_number',
+                'service_number',
+                'asset_number',
+                'barcode',
+                'network_name',
+                'assignee__friendly_name',
+                'assignee__full_name',
+                'borrower__friendly_name',
+                'borrower__full_name',
+                'owner__friendly_name',
+                'owner__full_name',
+                'home__short_name',
+                'home__full_name',
+                'location__short_name',
+                'location__full_name',
+        ]
+
+        self.vista_settings['filter_fields_available'] = [
             'mmodel',
-            'primary_id_field',
-            'serial_number',
-            'service_number',
-            'asset_number',
-            'barcode',
+            'mmodel__category',
             'condition',
-            'network_name',
-            'assignee',
-            'owner',
-            'borrower',
-            'home',
-            'location',
             'role',
+            'location',
+            'home',
             'latest_inventory'
-        ]:
-        showable_columns.append(
-            {
-                'name':fieldname,
-                'label':Item._meta.get_field(fieldname).verbose_name.title()
-            }
-        )
-        show_columns.append(fieldname)
+        ]
+
+        for fieldname in ['common_name', 'mmodel', 'primary_id', 'serial_number','service_number', 'latest_inventory']:
+            self.vista_settings['order_by_fields_available'].append(
+                { 'name':fieldname, 'label':Item._meta.get_field(fieldname).verbose_name.title() }
+            )
+            self.vista_settings['order_by_fields_available'].append(
+                { 'name':'-' + fieldname, 'label':'{} reverse'.format(Item._meta.get_field(fieldname).verbose_name.title()) }
+            )
+
+        for fieldname in [
+                'common_name',
+                'mmodel',
+                'primary_id_field',
+                'serial_number',
+                'service_number',
+                'asset_number',
+                'barcode',
+                'condition',
+                'network_name',
+                'assignee',
+                'owner',
+                'borrower',
+                'home',
+                'location',
+                'role',
+                'latest_inventory'
+            ]:
+            self.vista_settings['columns_available'].append(
+                {
+                    'name':fieldname,
+                    'label':Item._meta.get_field(fieldname).verbose_name.title()
+                }
+            )
+
+        return super().setup(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
 
     def get_queryset(self):
 
-        vista_object = get_vista_object(self, super().get_queryset(), 'libtekin.item' )
-        self.filter_object = vista_object['filter_object']
-        self.order_by = vista_object['order_by']
-        self.show_columns = vista_object['show_columns']
-        self.combined_text_search = vista_object['combined_text_search']
-        return vista_object['queryset']
+        self.vista_context = {
+            'show_columns':[],
+            'order_by':[],
+            'combined_text_search':'',
+        }
+
+        queryset = super().get_queryset()
+        vistaobj={'context':{}, 'queryset':queryset}
+        if 'vista_query_submitted' in self.request.POST:
+            vistaobj = make_vista(self.request, self.vista_settings, super().get_queryset())
+        elif 'get_vista' in self.request.POST:
+            vistaobj = retrieve_vista(self.request, self.vista_settings, super().get_queryset())
+
+        for key in vistaobj['context']:
+            self.vista_context[key] = vistaobj['context'][key]
+
+
+        queryset = vistaobj['queryset']
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
@@ -246,17 +273,25 @@ class ItemList(PermissionRequiredMixin, ListView):
         context_data['roles'] = Role.objects.all()
         context_data['locations'] = Location.objects.all()
         context_data['vistas'] = Vista.objects.filter(user=self.request.user, model_name='libtekin.item').all()
-        context_data['order_by_fields'] = self.order_by_fields
-        context_data['order_by'] = self.order_by
-        context_data['showable_columns'] = self.showable_columns
-        context_data['show_columns'] = self.show_columns
-        context_data['combined_text_search'] = self.combined_text_search
+        context_data['order_by_fields_available'] = self.vista_settings['order_by_fields_available']
+        context_data['columns_available'] = self.vista_settings['columns_available']
         context_data['ordering'] = Item._meta.ordering
 
-        if self.filter_object:
-            context_data['filter_object'] = self.filter_object
         if self.request.POST.get('vista__name'):
             context_data['vista__name'] = self.request.POST.get('vista__name')
+
+        if self.vista_context:
+            if 'filter' in self.vista_context:
+                for key in self.vista_context['filter']:
+                    context_data[key] = self.vista_context['filter'][key]
+
+        for key in [
+            'combined_text_search',
+            'text_fields_chosen',
+            'order_by'
+            ]:
+            if key in self.vista_context:
+                context_data[key] = self.vista_context[key]
 
         context_data['item_labels'] = { field.name: field.verbose_name.title() for field in Item._meta.get_fields() if type(field).__name__[-3:] != 'Rel' }
 
