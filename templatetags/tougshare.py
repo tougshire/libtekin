bdc06609f4cf6ec05013947be62e1c3ae6851e62
tougshire <tougshire@tougshire.com>
2022-02-20 13:05:38 -0500

    use settings.project_menu_template for global menu

--- templatetags/tougshare.py
+++ templatetags/tougshare.py
@@ -5,9 +5,10 @@ from django.conf import settings
 register = template.Library()
 
 @register.simple_tag
-def global_menu_template():
+def project_menu_template():
     try:
-        return settings.GLOBAL_MENU_TEMPLATE
+        print ('tp m2k959', settings.PROJECT_MENU_TEMPLATE)
+        return settings.PROJECT_MENU_TEMPLATE
     except:
         return None
 
