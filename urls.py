a8edabde00dbf50540d9f1a16527878a60cbb9df
tougshire <tougshire@tougshire.com>
2022-02-22 15:27:15 -0500

    add url redirects
    Import RecirectView  and reverse_lazy in urls.py, and add urls that redirect to other views

--- urls.py
+++ urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.views.generic.base import RedirectView
+from django.urls import path, reverse_lazy
 from . import views
 
 app_name = 'libtekin'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('libtekin:item-list'))),
+    path('item/', RedirectView.as_view(url=reverse_lazy('libtekin:item-list'))),
     path('item/create/', views.ItemCreate.as_view(), name='item-create'),
     path('item/<int:pk>/update/', views.ItemUpdate.as_view(), name='item-update'),
     path('item/<int:pk>/detail/', views.ItemDetail.as_view(), name='item-detail'),
@@ -11,18 +14,21 @@ urlpatterns = [
     path('item/list/', views.ItemList.as_view(), name='item-list'),
     path('item/<int:pk>/close/', views.ItemClose.as_view(), name="item-close"),
     path('item/<int:pk>/copy/', views.copy_item, name='item-copy'),
+    path('entity/', RedirectView.as_view(url=reverse_lazy('libtekin:entity-list'))),
     path('entity/create/', views.EntityCreate.as_view(), name='entity-create'),
     path('entity/<int:pk>/update/', views.EntityUpdate.as_view(), name='entity-update'),
     path('entity/<int:pk>/detail/', views.EntityDetail.as_view(), name='entity-detail'),
     path('entity/<int:pk>/delete/', views.EntityDelete.as_view(), name='entity-delete'),
     path('entity/list/', views.EntityList.as_view(), name='entity-list'),
     path('entity/<int:pk>/close/', views.EntityClose.as_view(), name="entity-close"),
+    path('location/', RedirectView.as_view(url=reverse_lazy('libtekin:location-list'))),
     path('location/create/', views.LocationCreate.as_view(), name='location-create'),
     path('location/<int:pk>/update/', views.LocationUpdate.as_view(), name='location-update'),
     path('location/<int:pk>/detail/', views.LocationDetail.as_view(), name='location-detail'),
     path('location/<int:pk>/delete/', views.LocationDelete.as_view(), name='location-delete'),
     path('location/list/', views.LocationList.as_view(), name='location-list'),
     path('location/<int:pk>/close/', views.LocationClose.as_view(), name="location-close"),
+    path('mmodel/', RedirectView.as_view(url=reverse_lazy('libtekin:mmodel-list'))),
     path('mmodel/create/', views.MmodelCreate.as_view(), name='mmodel-create'),
     path('mmodel/<int:pk>/update/', views.MmodelUpdate.as_view(), name='mmodel-update'),
     path('mmodel/<int:pk>/detail/', views.MmodelDetail.as_view(), name='mmodel-detail'),
